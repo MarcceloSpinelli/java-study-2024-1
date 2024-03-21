ff7ac1b8851f4202ac142c014cdb63702d09cf65
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2024-03-21 11:21:40 -0300

    improving3

--- tecnicas-de-programacao-web/sprint-02/projeto-jpa/projeto-jpa/src/main/java/school/sptech/projetojpa/MusicaController.java
+++ tecnicas-de-programacao-web/sprint-02/projeto-jpa/projeto-jpa/src/main/java/school/sptech/projetojpa/MusicaController.java
@@ -3,10 +3,10 @@ package school.sptech.projetojpa;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("musicas")
@@ -19,5 +19,33 @@ public class MusicaController {
     public ResponseEntity<Musica> criar(@RequestBody Musica novaMusica){
         Musica musicaSalva = musicaRepository.save(novaMusica);
         return ResponseEntity.status(200).body(musicaSalva);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Musica>> listar(){
+        List<Musica> musicas = musicaRepository.findAll();
+
+        if(musicas.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(musicas);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Musica> buscarPorId(@PathVariable int id) {
+        Optional<Musica> musicaOpt = musicaRepository.findById(id);
+
+        if (musicaOpt.isEmpty()){
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.status(200).body(musicaOpt.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable int id){
+        if(musicaRepository.existsById(id)){
+            musicaRepository.deleteById(id);
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(404).build();
     }
 }
